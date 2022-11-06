37b0e654d7a3bafd6368eba12eaea48d806e5953
Sandeep <sandeep.singh@squintmetrics.com>
2022-11-06 23:16:48 +0530

    Path changes

--- src/redux/saga/index.tsx
+++ src/redux/saga/index.tsx
@@ -1,12 +1,7 @@
 import { all, fork } from "redux-saga/effects";
-import * as AuthSaga from "./authSaga"; // Authentication Saga Handler
+import * as AuthSaga from "./auth-saga"; // Authentication Saga Handler
 import * as clinicSaga from "./clinicSaga";
 import * as doctorSaga from "./doctorSaga";
 export function* rootSaga() {
-  yield all([
-    fork(AuthSaga.AuthenticateUser),
-    // fork(AuthSaga.RegisterUser),
-    fork(clinicSaga.GetAllClinics),
-    fork(doctorSaga.GetDashboardDoctorList)
-  ]);
+  yield all([fork(AuthSaga.AuthenticateUser), fork(clinicSaga.GetAllClinics), fork(doctorSaga.GetDashboardDoctorList)]);
 }
