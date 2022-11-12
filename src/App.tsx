522f750caa46fde06ce377aa87ea35d3c648b67c
Arif <arif@Arifs-MacBook-Pro.local>
2022-11-13 01:08:13 +0530

    loader added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import "./App.css";
 import { AppRouting } from "./router/appRouting";
 import { AppLoaderContext } from "./contexts";
+import loader from "../src/assets/dummy/loader-spinner.svg";
 
 function App() {
   const { isAppLoader } = React.useContext(AppLoaderContext);
@@ -10,14 +11,14 @@ function App() {
       {/* <ToastContainer /> */}
       {isAppLoader && (
         <div className="loader">
-          <p>Loading...</p>
+          <img src={loader} alt="loader" />
           {/* <Loader type="Puff" color="#00BFFF" height={100} width={100}  /> */}
         </div>
       )}
       <Suspense
         fallback={
           <div className="loader">
-            <p>Loading...</p>
+            <img src={loader} alt="loader" />
           </div>
         }
       >
