5e029329ce626f47b428a9eeb1a93b315d732fa0
Arif <arif@Arifs-MacBook-Pro.local>
2022-11-16 23:06:10 +0530

    how it works and footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,17 +4,18 @@ import clicks from "../../../../assets/dummy/clicks.jpg";
 import pre from "../../../../assets/dummy/pre.jpg";
 import feedback from "../../../../assets/dummy/feedback11.jpg";
 import prescription from "../../../../assets/dummy/prescription.jpg";
+import footerLogo from "../../assets/dummy/doctor-logo.svg";
 
 const Footer = () => {
   return (
     <>
       <div className="footer-logo">
-        <img className="header-img" src="" alt="footer-logo" />
-        <ul>
-          <li>{/* <ion-icon name="logo-twitter"></ion-icon> */}</li>
-          <li>{/* <ion-icon name="logo-facebook"></ion-icon> */}</li>
-          <li>{/* <ion-icon name="logo-instagram"></ion-icon> */}</li>
-        </ul>
+        <img className="footer-img" src={footerLogo} alt="footer-logo" />
+        {/* <ul>
+          <li><ion-icon name="logo-twitter"></ion-icon></li>
+          <li><ion-icon name="logo-facebook"></ion-icon></li>
+          <li><ion-icon name="logo-instagram"></ion-icon></li>
+        </ul> */}
         <p>
           Copyright &copy; <span className="year">2027</span> by Dr practo Inc.All right reserved
         </p>
